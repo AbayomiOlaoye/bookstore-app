e7cfc52c749f9ad21b43aad9e5666d50a0db1241
Oluwatoyin Olaoye <mindprepopps@gmail.com>
2022-11-19 20:39:35 +0100

    Connect to api and create getBooks function

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,17 +1,26 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
+const GET_BOOK = 'bookstore/books/GET_BOOK';
 
 const prevState = [];
+const source = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/BoLpCLs90yg1VqJcibSz/books';
 
-// Action Logic
-const addBook = (book) => ({
-  type: ADD_BOOK,
-  book,
-});
+/* Actions Logic */
 
-const removeBook = (id) => ({
-  type: REMOVE_BOOK,
-  id,
+// Fetch Books
+const getBooks = createAsyncThunk(GET_BOOK, async (post, { dispatch }) => {
+  const res = await fetch(source);
+  const data = await res.json();
+  const books = Object.keys(data).map((key) => ({
+    ...data[key][0],
+    item_id: key,
+  }));
+  dispatch({
+    type: GET_BOOK,
+    books,
+  });
 });
 
 // Book Reducer logic
@@ -30,5 +39,5 @@ const BooksRedux = (state = prevState, action) => {
     default: return state;
   }
 };
-export { addBook, removeBook };
+export { getBooks, addBook, removeBook };
 export default BooksRedux;
